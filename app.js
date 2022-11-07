b7f5392daa93272c5b395328b79bac0e665eadea
Andrei Korneev <andie.kor@yandex.ru>
2022-11-07 12:28:18 +0300

    Production

--- app.js
+++ app.js
@@ -10,11 +10,30 @@ app.use(express.json({ extended: true }))
 app.use('/api/articles', require('./routes/article.routes'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    if (req.headers.host === 'jadoo.andrey-kor.ru') {
+        app.use('/', express.static(path.join(__dirname, 'jadoo')))
+        
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname, 'jadoo', 'index.html'))
+        })
+    }
+
+    else if (req.headers.host === 'bangkok.andrey-kor.ru') {
+        app.use('/', express.static(path.join(__dirname, 'bangkok')))
+        
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname, 'bangkok', 'index.html'))
+        })
+    }
+
+    else {
+        app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        })
+    }    
 }
 
 const PORT = config.get('port') || 5000
